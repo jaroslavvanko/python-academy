74dec399bb22dd6ffd292db390ac4e90a7377b40
Jaroslav Vanko <jaroslavvanko@seznam.cz>
2019-10-26 13:50:35 +0200

    lekce01exe04

---
+++ lekce01/04_slicing.py
@@ -0,0 +1,9 @@
+# extract and print first five letters
+first_five = "indexing"[:5]
+print("First five letters:", first_five)
+# extract and prinnt last five letters
+last_five = "indexing"[-5:]
+print("Last five letters:", last_five)
+# extract and print each 3 letters
+third = "indexing"[::3]
+print("Each third letter:", third)
