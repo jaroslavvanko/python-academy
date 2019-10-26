3981cf509c03b1b998c3fd9d9dccb727bb846c3b
Jaroslav Vanko <jaroslavvanko@seznam.cz>
2019-10-26 15:24:48 +0200

    lekce01exe07

---
+++ lekce01/07_create_list.py
@@ -0,0 +1,13 @@
+# create and print candidates
+candidates = []
+print("Candidates at the beginning:", candidates)
+# create employees and print employees
+employees = ["Anna", "Jacob", "Martin"]
+print("Employees at the beginning:", employees)
+# add new candidates and print them
+candidates.append("Carlos")
+candidates.append("Peter")
+print("Added new candidates to the list:", candidates)
+# add new employees and print
+employees.insert(2, "Bruno")
+print("Added new employees:", employees)
